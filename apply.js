f278dbb0a7d7004707f2d1f12cc999dbede06150
bambi15 <alivrabel007@gmail.com>
2025-05-01 19:57:13 +0200

    added checks to application

--- apply.js
+++ apply.js
@@ -6,18 +6,49 @@ document
     const hra = document.querySelector('input[name="radio"]:checked')?.id;
 
     const data = {
-      meno: document.querySelector("#meno").value,
-      priezvisko: document.querySelector("#priezvisko").value,
-      email: document.querySelector("#email").value,
-      skola: document.querySelector('input[placeholder="tvoja škola"]').value,
-      rocnik: document.querySelector(
-        'input[placeholder="ktorý ročník navštevuješ"]'
-      ).value,
+      meno: document.querySelector("#meno").value.trim(),
+      priezvisko: document.querySelector("#priezvisko").value.trim(),
+      email: document.querySelector("#email").value.trim(),
+      skola: document
+        .querySelector('input[placeholder="tvoja škola"]')
+        .value.trim(),
+      rocnik: document
+        .querySelector('input[placeholder="ktorý ročník navštevuješ"]')
+        .value.trim(),
       hra: hra,
-      rank: document.querySelector('input[placeholder^="SoloQ"]').value,
-      link: document.querySelector('input[placeholder^="OP.GG"]').value,
-      o_sebe: document.querySelector("textarea").value,
+      rank: document.querySelector('input[placeholder^="SoloQ"]').value.trim(),
+      link: document.querySelector('input[placeholder^="OP.GG"]').value.trim(),
+      o_sebe: document.querySelector("textarea").value.trim(),
     };
+
+    // Validation: Check if any field is empty
+    if (
+      !data.meno ||
+      !data.priezvisko ||
+      !data.email ||
+      !data.skola ||
+      !data.rocnik ||
+      !data.hra ||
+      !data.rank ||
+      !data.link ||
+      !data.o_sebe
+    ) {
+      alert("Prosím, vyplňte všetky polia.");
+      return;
+    }
+
+    // Email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(data.email)) {
+      alert("Zadajte platnú emailovú adresu.");
+      return;
+    }
+
+    // Link validation
+    if (!data.link.startsWith("https://")) {
+      alert("Zadajte platný odkaz");
+      return;
+    }
 
     fetch("https://sheetdb.io/api/v1/67kd5sfcubfrs", {
       method: "POST",
